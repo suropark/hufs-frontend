76241b3b2d16718b067dc124d850f57de7ecf900
suropark <52706443+suropark@users.noreply.github.com>
2021-04-02 22:28:27 +0900

    [ 박수로 ] 버그픽스 : JSX 태그 에러로 인한 임시 조치

--- src/components/map/mapSection/SearchBar.js
+++ src/components/map/mapSection/SearchBar.js
@@ -1,22 +1,18 @@
-import React from "react";
-import { FaSearch } from "react-icons/fa";
+import React from 'react';
+// import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ onChange, placeholder }) => {
   return (
     <div className="Search">
-
       <input
         className="SearchInput"
         type="text"
         onChange={onChange}
         placeholder={placeholder}
       />
-      <span className="SearchSpan">
-        <FaSearch />
-      </span>
+      <span className="SearchSpan">{/* <FaSearch /> */}</span>
     </div>
   );
 };
-
 
 export default SearchBar;
