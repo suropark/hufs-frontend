dcfb24be97c1bea27fafbe95952c77485a639a73
NamNamju <namuju_hi@naver.com>
2021-04-09 21:53:19 +0900

    [ 황남주 ] 기능 수정 : 지도 서울/글로벌 구분

--- src/components/map/mapSection/KakaoMap.js
+++ src/components/map/mapSection/KakaoMap.js
@@ -3,20 +3,20 @@ import { useDispatch } from 'react-redux';
 
 const { kakao } = window;
 
-const KakaoMap = () => {
+const KakaoMap = ({lat, lng}) => {
   const dispatcher = useDispatch();
 
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
-      center: new kakao.maps.LatLng(37.59732049638715, 127.05882833955489), // 한국외대 설캠
+      center: new kakao.maps.LatLng(lat,lng),
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
 
 
 
-  }, []);
+  }, [lat,lng]);
 
 
 
