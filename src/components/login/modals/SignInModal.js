93c67fc1893b757077c30aadcd3d71ea50946ea6
ga0-c1028 <gayoung.rho@gmail.com>
2021-04-13 20:49:36 +0900

    [ 노가영 ] 기능 수정 : 카카오 로그인

--- src/components/login/modals/SignInModal.js
+++ src/components/login/modals/SignInModal.js
@@ -1,94 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { PUBLIC_IP } from '../../../config';
-import axios from 'axios';
 import { message, Modal, Button } from 'antd';
 import kakao_pic from '../style/kakao_pic.png';
-import google_pic from '../style/google_pic.png';
-import GoogleSignIn from '../social/GoogleSignIn';
-import { useHistory } from 'react-router-dom';
-import ReactGoogle from '../social/ReactGoogle';
+import GoogleSignIn  from '../social/GoogleSignIn';
+import KakaoSignIn from '../social/KakaoSignIn';
+// import google_pic from '../style/google_pic.png';
+// import { useHistory } from 'react-router-dom';
+// import axios from 'axios';
+// import { PUBLIC_IP } from '../../../config';
+
 function SignInModal() {
-
   const [ modalVisible, setModalVisible ] = useState(false);
-  const [ emailInfo, setEmailInfo ] = useState({ email: '', provider: '', });
-  const { Kakao } = window;
-  const history = useHistory();
-  
-
-  const signInKakao = async (e) => {
-    console.log(e);
-    console.log('hi');
-    //const kakaoInit = () => {
-    Kakao.init('690082dcedf6efeca17e320160913cb3');
-    console.log(Kakao.isInitialized());
-    //}
-    Kakao.Auth.authorize({
-      redirectUri: `${PUBLIC_IP}/user/sign-in`,
-    });
-
-    // setEmailInfo({ ... emailInfo, email: e.target})
-    // console.log(emailInfo);
-
-    const request = await axios
-
-    .get(`${PUBLIC_IP}/user/sign-in`, emailInfo)
-    .then((response) => {
-      message.success("로그인이 정상 완료 되었습니다.")
-      history.push('/');
-    })
-    .catch((error) => {
-      switch (error.response?.status) {
-        case 404:
-          message.error("회원가입이 되지 않은 사용자입니다. 회원가입 페이지로 넘어갑니다.")
-          history.push('/redirect');
-      }
-    })
-  }
-
-  //  const authorization = async (e) => {
-  //    console.log("here");
-  //    console.log(e);
-  //    const request = await axios
-  //    .post(`${PUBLIC_IP}/user/sign-in/kakao`, emailInfo)
-  //    .then((response) => {
-  //     message.success("로그인이 정상 완료 되었습니다.")
-  //     history.push('/');
-  //    })
-  //    .catch((error) => {
-  //      switch (error.response?.status) {
-  //        case 404:
-  //          message.error("회원가입이 되지 않은 사용자입니다. 회원가입 페이지로 넘어갑니다.")
-  //          history.push('/redirect') //회원가입 페이지로 넘겨주기
-  //      }
-  //    })
-  // }
-  
+  // const [ emailInfo, setEmailInfo ] = useState({ email: '', provider: '', });
+  // const { Kakao } = window;
+  // const history = useHistory();
 
   return (
-    <>
-      <Button type="text" onClick={() => setModalVisible(true)}>
-        로그인
-      </Button>
-      <Modal
-        title="로그인 / LOGIN"
-        centered
-        okButtunProps={{ style: { display: 'none' } }}
-        visible={modalVisible}
-        onOk={() => setModalVisible(false)}
-        onCancel={() => setModalVisible(false)}
-      >
-        {/* <GoogleSignIn /> */}
-        <ReactGoogle setModalVisible={setModalVisible} />
-        <a id="custom-login-kakaoBtn" href="javascript:loginWithKakao()">
-          <img
-            style={{ cursor: 'pointer' }}
-            onClick={signInKakao}
-            src={kakao_pic}
-          />
-        </a>
-      </Modal>
-    </>
-  );
+   <>
+    <Button type="text" onClick={() => setModalVisible(true)}>
+      로그인
+    </Button>
+    <Modal
+    title="로그인 / LOGIN"
+    centered
+    okButtunProps={{ style: { display: 'none' } }}
+    visible = {modalVisible}
+    onOk = {() => setModalVisible(false)}
+    onCancel = {() => setModalVisible(false)}
+    >
+      <GoogleSignIn/>
+      <img style={{ cursor: 'pointer' }}
+        onClick={KakaoSignIn}
+        src={kakao_pic}
+      />
+    </Modal>
+   </>
+ )
 }
 
 export default SignInModal;
