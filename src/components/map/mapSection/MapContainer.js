74b17df72264a4f648abaf36e1ba86f3a9d28894
NamNamju <namuju_hi@naver.com>
2021-04-14 00:49:14 +0900

    [ 황남주 ] 기능 수정 : 지도 버튼 눌렀을 시 수정

--- src/components/map/mapSection/MapContainer.js
+++ src/components/map/mapSection/MapContainer.js
@@ -41,19 +41,21 @@ const MapContainer = ({match}) => {
 
         <div className="content" >
 
-          <div id="seoul" defaultSelectedKeys={['1']}>
-            <Button type="text" id="button-head" key="1">Seoul</Button>
-            <Button type="text" onClick={(e) => {setData(storeSeoul);
+          <div id="seoul" defaultSelectedKeys={['1']}
+          onClick={(e) => {setData(storeSeoul);
             setLat(37.59732049638715);
-          setLng(127.0588283395548)}} >맛집공간</Button>
+          setLng(127.0588283395548)}}>
+            <Button type="text" id="button-head" key="1">Seoul</Button>
+            <Button type="text"  >맛집공간</Button>
 
           </div>
-          <div id="global" defaultSelectedKeys={['2']}>
-            <Button type="text" id="button-head" key="2">Global</Button>
-            <Button type="text" onClick={(e) => {setData(storeGlobal);
+          <div id="global" defaultSelectedKeys={['2']}
+          onClick={(e) => {setData(storeGlobal);
             setLat(37.336538181222245);
             setLng(127.25253858610613);
-            }}>맛집공간</Button>
+            }}>
+            <Button type="text" id="button-head" key="2">Global</Button>
+            <Button type="text" >맛집공간</Button>
 
           </div>
 
