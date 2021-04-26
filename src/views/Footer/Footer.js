af7c9355ca2039d7aa53408e803a68d49dd98de0
xogns9578 <xogns9578@gmail.com>
2021-04-27 01:20:13 +0900

    [ 기능추가 ] 푸터: Carousel 수정중

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Slide2 } from '../../components/slide/Slide'
 
 function Footer() {
   return (
     <>
-      <div className="Footer"></div>
+      <div className="Footer">
+        {/* <Slide2 /> */}
+      </div>
     </>
   );
 }
