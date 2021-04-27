c0f66873aad17741f1b4ca0cb51629f5d215b975
xogns9578 <xogns9578@gmail.com>
2021-04-27 14:47:43 +0900

    [ 기능추가 ] 퀵메뉴 고정

--- src/views/Quick/Quick.js
+++ src/views/Quick/Quick.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Affix } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
@@ -12,33 +12,26 @@ function Quick() {
     console.log('click ', e);
   };
   return (
-    <Menu
-      onClick={handleClick}
+    <Affix offsetTop={250} >
+      <Menu
+        onClick={handleClick}
 
 
-      mode="inline"
-    >
-      <SubMenu icon={<MailOutlined />} title="Quick Menu">
-        <Menu.Item key="1"><a href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
-          target="_blank"
-          title="종합정보시스템">종정시</a></Menu.Item>
-        <Menu.Item key="10"><a title="시간표">시간표</a></Menu.Item>
-        <Menu.Item key="11"><a title="장학 게시판">장학 게시판</a></Menu.Item>
-        <Menu.Item key="12"><a title="과 게시판">과 게시판</a></Menu.Item>
-      </SubMenu>
-    </Menu >
+        mode="inline"
+      >
+        <SubMenu icon={<MailOutlined />} title="Quick Menu">
+          <Menu.Item key="1"><a href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
+            target="_blank"
+            title="종합정보시스템">종정시</a></Menu.Item>
+          <Menu.Item key="10"><a title="시간표">시간표</a></Menu.Item>
+          <Menu.Item key="11"><a title="장학 게시판">장학 게시판</a></Menu.Item>
+          <Menu.Item key="12"><a title="과 게시판">과 게시판</a></Menu.Item>
+        </SubMenu>
+      </Menu >
+    </Affix>
 
   );
 }
-{/* <Anchor>
-        <Link
-          href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
-          target="_blank"
-          title="종합정보시스템"
-        />
-        <Link href="#components-anchor-demo-static" title="시간표" />
-        <Link href="#Anchor-Props" title="장학 게시판" />
-        <Link href="#Link-Props" title="과 게시판" />
-      </Anchor> */}
+
 
 export default Quick;
