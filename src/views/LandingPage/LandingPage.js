04ebfc5a447d1c5304b8a25655c416ce8a3ec748
xogns9578 <xogns9578@gmail.com>
2021-04-27 16:07:41 +0900

    [ 이태훈 ] 메인 페이지 반응형 전부 수정

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -7,12 +7,16 @@ import Slide from '../../components/slide/Slide';
 import SearchAll from '../../components/post/SearchAll';
 import CalendarMainPage from '../../components/calendar/CalendarMainPage';
 function LandingPage(props) {
+  const opner = 'https://hufspace.com/';
+
   return (
     <div>
       <Header />
-      <Quick />
+
       <div className="Main">
         <SearchAll />
+        {opner === 'https://hufspace.com/' ? <Quick /> : null}
+        {/* <Quick /> */}
         <div className="Mainbanner">
 
           <Slide />
--- src/views/Quick/Quick.js
+++ src/views/Quick/Quick.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Menu, Affix } from 'antd';
+import { Menu } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 
 function Quick() {
+  // const [forMain, setforMain] = useState(true)
 
 
 
   const handleClick = e => {
     console.log('click ', e);
   };
+
   return (
-    <Affix offsetTop={250} >
-      <Menu
-        onClick={handleClick}
+
+    <Menu
+      onClick={handleClick}
 
 
-        mode="inline"
-      >
-        <SubMenu icon={<MailOutlined />} title="Quick Menu">
-          <Menu.Item key="1"><a href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
-            target="_blank"
-            title="종합정보시스템">종정시</a></Menu.Item>
-          <Menu.Item key="10"><a title="시간표">시간표</a></Menu.Item>
-          <Menu.Item key="11"><a title="장학 게시판">장학 게시판</a></Menu.Item>
-          <Menu.Item key="12"><a title="과 게시판">과 게시판</a></Menu.Item>
-        </SubMenu>
-      </Menu >
-    </Affix>
+      mode="inline"
+    >
+      <SubMenu icon={<MailOutlined />} title="Quick Menu">
+        <Menu.Item key="1"><a href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
+          target="_blank"
+          title="종합정보시스템">종정시</a></Menu.Item>
+        <Menu.Item key="10"><a title="시간표">시간표</a></Menu.Item>
+        <Menu.Item key="11"><a title="장학 게시판">장학 게시판</a></Menu.Item>
+        <Menu.Item key="12"><a title="과 게시판">과 게시판</a></Menu.Item>
+      </SubMenu>
+    </Menu >
+
 
   );
 }
