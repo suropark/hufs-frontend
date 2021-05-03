b660c90decaa321f40ab9fe7e1be4fe4c5aba881
xogns9578 <xogns9578@gmail.com>
2021-05-04 01:34:25 +0900

    [ 이태훈 ] 기능추가 : footer 수정

--- src/components/slide/Slide.js
+++ src/components/slide/Slide.js
@@ -2,6 +2,14 @@ import img1 from '../../image/banner/banner1.png';
 import img2 from '../../image/banner/banner2.png';
 import img3 from '../../image/banner/banner3.png';
 import img4 from '../../image/banner/banner4.png';
+// 배너
+import foo1 from '../../image/footer-icon/hufspace-insta.png';
+import foo2 from '../../image/footer-icon/hufstagram.png';
+import foo3 from '../../image/footer-icon/Hufs-face.png';
+import foo4 from '../../image/footer-icon/Hufs-you.png';
+import foo5 from '../../image/footer-icon/forHufs.png';
+import foo6 from '../../image/footer-icon/On.png';
+// 푸터
 import test from '../../image/test1.png';
 
 import 'slick-carousel/slick/slick.css';
@@ -61,48 +69,46 @@ export function Slide2() {
     <Slider {...footerSetting}>
       <ul class="hufs_link">
         <li>
-          <a href="http://www.hufs.ac.kr/" target="_blank"><img src={test} alt="error" /></a>
+          <a href="http://www.hufs.ac.kr/" target="_blank"><img src={foo1} alt="error" /></a>
           <div class="info">
-            <span class="name">한국외대 공식 홈페이지</span>
+            <span class="name">HUFSpace</span>
+            <span class="info2">HUFSpace 공식 인스타그램</span>
           </div>
 
         </li>
         <li>
-          <a href="https://eclass.hufs.ac.kr/" target="_blank"><img src={test} alt="error" /></a>
+          <a href="https://eclass.hufs.ac.kr/" target="_blank"><img src={foo2} alt="error" /></a>
           <div class="info">
-            <span class="name">한국외대 E-class</span>
+            <span class="name">Hufstagram</span>
+            <span class="info2">한국외대 공식 인스타그램</span>
           </div>
         </li>
         <li>
-          <a href="https://www.facebook.com/hufsstudent/" target="_blank"><img src={test} alt="error" /></a>
+          <a href="https://www.facebook.com/hufsstudent/" target="_blank"><img src={foo3} alt="error" /></a>
+          <div class="info">
+            <span class="name">HUFS 페이스북</span>
+            <span class="info2">한국외대 공식 페이스북</span>
+          </div>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/hufsglobal42th" target="_blank"><img src={foo4} alt="error" /></a>
+          <div class="info">
+            <span class="name">HUFS 유튜브</span>
+            <span class="info2">한국외대 공식 유튜브</span>
+          </div>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/hufspr" target="_blank"><img src={foo5} alt="error" /></a>
           <div class="info">
             <span class="name">서울캠퍼스 총학생회</span>
-            <span class="info2">54대 총학생회 새벽으로 부터</span>
+            <span class="info2">제55대 총학생회 '외대에게'</span>
           </div>
         </li>
         <li>
-          <a href="https://www.facebook.com/hufsglobal42th" target="_blank"><img src={test} alt="error" /></a>
+          <a href="https://www.youtube.com/user/HUFS1954" target="_blank"><img src={foo6} alt="error" /></a>
           <div class="info">
-            <span class="name">한국외대 공식 홈페이지</span>
-            <span class="info2">42대 총학생회 On</span>
-          </div>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/hufspr" target="_blank"><img src={test} alt="error" /></a>
-          <div class="info">
-            <span class="name">한국외대 공식 Facebook</span>
-          </div>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/user/HUFS1954" target="_blank"><img src={test} alt="error" /></a>
-          <div class="info">
-            <span class="name">한국외대 공식 Youtube</span>
-          </div>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/hufstagram/" target="_blank"><img src={test} alt="error" /></a>
-          <div class="info">
-            <span class="name">한국외대 공식 Instagram</span>
+            <span class="name">글로벌캠퍼스 총학생회</span>
+            <span class="info2">제42대 총학생회 'ON'</span>
           </div>
         </li>
       </ul>
