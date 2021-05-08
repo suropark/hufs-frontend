fff6fbc77fd010f057c5bc2d1e71a19cef1c9ba5
xogns9578 <xogns9578@gmail.com>
2021-05-09 00:52:22 +0900

    [ 이태훈  ] 디자인 : 퀵메뉴 메인화면 특수 지정

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ import Footer from '../Footer/Footer';
 import Slide from '../../components/slide/Slide';
 import SearchAll from '../../components/post/SearchAll';
 import CalendarMainPage from '../../components/calendar/CalendarMainPage';
+import styles from '../../css/LandingPage.module.css';
 function LandingPage(props) {
   // const now_url = window.location.href;
   // console.log(now_url);
@@ -17,7 +18,10 @@ function LandingPage(props) {
       <div className="Main">
         <SearchAll />
         {/* {now_url === 'https://hufspace.com/' ? <Quick /> : null} */}
-        <Quick />
+        <div
+          className={styles.quick}>
+          <Quick />
+        </div>
         <div className="Mainbanner">
 
           <Slide />
--- src/views/Quick/Quick.js
+++ src/views/Quick/Quick.js
@@ -22,9 +22,9 @@ function Quick() {
         <Menu.Item key="1"><a href="https://wis.hufs.ac.kr/src08/jsp/index.jsp"
           target="_blank"
           title="종합정보시스템">종정시</a></Menu.Item>
-        <Menu.Item key="10"><a title="시간표">시간표</a></Menu.Item>
-        <Menu.Item key="11"><a title="장학 게시판">장학 게시판</a></Menu.Item>
-        <Menu.Item key="12"><a title="과 게시판">과 게시판</a></Menu.Item>
+        <Menu.Item key="2"><a title="시간표">시간표</a></Menu.Item>
+        <Menu.Item key="3"><a title="장학 게시판">장학 게시판</a></Menu.Item>
+        <Menu.Item key="4"><a title="과 게시판">과 게시판</a></Menu.Item>
       </SubMenu>
     </Menu >
 
