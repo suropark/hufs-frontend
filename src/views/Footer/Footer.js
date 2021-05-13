1e012439892aea7901bd588bacabe103822f8322
xogns9578 <xogns9578@gmail.com>
2021-05-13 15:19:19 +0900

    [ 이태훈 ] Footer : 하단 약관 추가

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Slide2 } from '../../components/slide/Slide'
-import Rule from '../../components/rule/Rule'
+import InformationModal from '../../components/rule/InformationModal'
+import UseModal from '../../components/rule/UseModal'
 function Footer() {
   return (
     <>
       <div className="Footer">
         <Slide2 />
-        <Rule />
+        <div className="rule">
+          <InformationModal />
+          <UseModal />
+        </div>
+
       </div>
     </>
   );
